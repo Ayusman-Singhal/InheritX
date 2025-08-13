41d9c683367619812089b1448aa932f31c15520f
Ayusman-Singhal <ayusmansinghal@gmail.com>
2025-08-13 07:51:42 +0000

    Deploying to gh-pages from @ Ayusman-Singhal/InheritX@3c16fd61c7002fd5e7447ecf5bcf4f56a32520f4 🚀

---
+++ _next/static/chunks/pages/_app-39a36748d4d45d35.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{7522:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8176)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(7522),n(2558))),_N_E=_.O()}]);
